d07c53970667c0c5b3bdeb728e6c7ca6492d671d
lucasgalarce <galarcelucas@gmail.com>
2022-10-28 12:56:54 -0300

    change status error number

--- node/src/middlewares/auth.ts
+++ node/src/middlewares/auth.ts
@@ -8,6 +8,6 @@ export const auth = (req, res, next) => {
     req.user = verified;
     return next();
   } catch (error) {
-    res.status(400).json({ error: "Invalid token" });
+    res.status(401).json({ error: "Invalid token" });
   }
 };
