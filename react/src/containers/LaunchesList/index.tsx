fd204c14b0cb37949a36230ca7e9e13103a93210
lucasgalarce <galarcelucas@gmail.com>
2022-10-30 22:56:52 -0300

    loading

--- react/src/containers/LaunchesList/index.tsx
+++ react/src/containers/LaunchesList/index.tsx
@@ -12,6 +12,7 @@ export const LaunchesList = () => {
   const [searchText, setSearchText] = useState<string>("");
   const { showAll } = useContext(ModeContext);
   const [currentPage, setCurrentPage] = useState<number>(1);
+  const [loading, setLoading] = useState(false);
   const { favorites, updateFavorite } = useLocalStorage()
   const filterLaunches = () => {
     setFilteredLaunches(
@@ -21,14 +22,16 @@ export const LaunchesList = () => {
 
   const loadLaunches = async () => {
     try {
-        const launches = await getLaunches();
-        setLaunches(launches);
+      const launches = await getLaunches();
+      setLaunches(launches);
+      setLoading(false)
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
+    setLoading(true)
     loadLaunches();
   }, []);
 
@@ -41,27 +44,33 @@ export const LaunchesList = () => {
         <span>Total ({filteredLaunches.length})</span>
         <Search value={searchText} onChange={setSearchText} />
       </div>
-      <div className="launches-list">
-        {filteredLaunches
-          .filter(
-            (_: Launch, i: number) =>
-              i >= CARDS_PER_PAGE * (currentPage - 1) &&
-              i < CARDS_PER_PAGE * currentPage
-          )
-          .map((launch, i) => (
-            <LaunchCard
-              key={launch.flight_number}
-              launch={launch}
-              updateFavorite={updateFavorite}
-              favorites={favorites}
+      {loading ? <div className="loading">Loading...</div> : 
+        (
+          <>
+            <div className="launches-list">
+            {filteredLaunches
+              .filter(
+                (_: Launch, i: number) =>
+                  i >= CARDS_PER_PAGE * (currentPage - 1) &&
+                  i < CARDS_PER_PAGE * currentPage
+              )
+              .map((launch, i) => (
+                <LaunchCard
+                  key={launch.flight_number}
+                  launch={launch}
+                  updateFavorite={updateFavorite}
+                  favorites={favorites}
+                />
+              ))}
+            </div>
+            <Pagination
+              value={currentPage}
+              onChange={setCurrentPage}
+              itemsCount={filteredLaunches.length}
             />
-          ))}
-      </div>
-      <Pagination
-        value={currentPage}
-        onChange={setCurrentPage}
-        itemsCount={filteredLaunches.length}
-      />
+          </>
+        )
+      }
     </div>
   );
 };
